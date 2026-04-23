df9e7fe8dd0a0a61aaedd9d9e6dda4e572159f44
Ajay B L <be.el.ajay@gmail.com>
2026-04-23 18:41:51 +0530

    Add production URL to CORS origins for deployment

--- app/main.py
+++ app/main.py
@@ -26,6 +26,7 @@ app = FastAPI(
 origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://shiftready-ui-12644234558.australia-southeast1.run.app"
 ]
 
 app.add_middleware(
